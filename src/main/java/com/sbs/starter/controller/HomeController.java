04dfb04fedf44e0495f6f02ebcd5c9fed10ffb9c
pkt <kyutpark@naver.com>
2020-03-12 13:50:20 +0900

    프로젝트 생성 및 기초세팅

---
+++ src/main/java/com/sbs/starter/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.sbs.starter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "메인화면입니다.";
+	}
+}
