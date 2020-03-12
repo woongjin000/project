5ef04517b7d79f896c880ee6f500e4a7751fc784
pkt <kyutpark@naver.com>
2020-03-12 14:34:07 +0900

    doAdd 서비스 구현

--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.sbs.starter.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,10 @@ public class ArticleServiceImpl implements ArticleService {
 	
 	public List<Article> getList() {
 		return articleDao.getList();
+	}
+
+	@Override
+	public void add(Map<String, Object> param) {
+		articleDao.add(param);
 	}
 }
