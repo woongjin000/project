09bba27a7b3444e0c24640d26d5b4502a091814d
pkt <kyutpark@naver.com>
2020-03-12 13:56:16 +0900

    ArticleService, ArticleServiceImpl, ArticleDto 생성 및 세팅, getList()함수 구현

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -1,13 +1,22 @@
 package com.sbs.starter.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sbs.starter.dto.Article;
+import com.sbs.starter.service.ArticleService;
+
 @Controller
 public class ArticleController {
+	@Autowired
+	ArticleService articleService;
 	
 	@RequestMapping("article/list")
 	public String showList() {
+		List<Article> list = articleService.getList();
 		return "article/list";
 	}
 }
